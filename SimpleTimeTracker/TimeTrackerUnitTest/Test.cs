631b6608722431e5137c5b6f582c1eac7fac081c
vbalage <balazs.viszoczki@gmail.com>
2017-01-26 23:43:47 +0100

    NUnit test project added

---
+++ SimpleTimeTracker/TimeTrackerUnitTest/Test.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using TimeTracker.Core.DataTypes;
+
+namespace TimeTrackerUnitTest
+{
+	[TestFixture]
+	public class Test
+	{
+		Task t;
+
+		[SetUp]
+		public void Init()
+		{
+			t = new Task();
+		}
+
+		[Test()]
+		public void PropertyTest()
+		{
+			var taskName = "TestTask";
+			t.Name = taskName;
+			StringAssert.AreEqualIgnoringCase(t.Name, taskName);
+		}
+	}
+}
